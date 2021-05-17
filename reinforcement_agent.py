d3bf13a82d259a76ed462255865cd66d8cced2fe
Joakim Olsson <joakim.michael.olsson@gmail.com>
2021-05-17 14:28:09 +0200

    adding training visualisation

--- reinforcement_agent.py
+++ reinforcement_agent.py
@@ -5,6 +5,8 @@ import math
 import re
 import json
 import os
+import datetime
+import time
 
 from dealer import Dealer
 
@@ -90,7 +92,8 @@ class Agent:
             mov_average[i] = avg
         return mov_average
 
-    def train_agent(self, num_of_episodes=1000000):
+    def train_agent(self, num_of_episodes=1000000, azim_start=0, extract_performance_every=np.inf):
+        azim = azim_start                            # Azim angle start, used for performance visualisation
         q = self.q                                   # Get Q-table
         explore_stop = round(num_of_episodes * 0.8)  # Stop exploration after 80% of training
 
@@ -98,8 +101,10 @@ class Agent:
         n = 0                                        # Print-out tick count
         print_tick = 0                               # Episode tick count
         outcome_list = [0] * num_of_episodes         # List holding episode outcomes
+        performance_count = 0
         for i_episode in range(0, num_of_episodes):
             n += 1
+            performance_count += 1
             if n >= print_tick_lim:
                 print_tick += n
                 n = 0
@@ -156,11 +161,37 @@ class Agent:
                 if i_episode < explore_stop:
                     action = self.change_action(action, self.exploration_prob)
             outcome_list[i_episode] = reward
+            if performance_count > extract_performance_every:
+                performance_count = 0
+                o = outcome_list[i_episode - extract_performance_every + 1:i_episode]
+                self.visualize_progress(o, i_episode, azim)
+                azim += 1
 
         # ---- Save q-table ----
         with open('Q.json', 'w') as outfile:
             json.dump(q, outfile)
         self.q = q
+
+        # TODO: Put this in a function
+        # bin_size = 1000
+        # n = math.ceil(len(outcome_list) / bin_size)
+        # bin_count = 0
+        # X = []
+        # Y = []
+        # for i in range(n):
+        #     if i < n - 1:
+        #         outcome = np.array(outcome_list[bin_count:bin_count + bin_size])
+        #     else:
+        #         outcome = np.array(outcome_list[bin_count:-1])
+        #     idx = np.where(outcome != 0)[0]
+        #     outcome = np.array([o for i, o in enumerate(outcome) if i in idx])
+        #     n_win = np.where(outcome > 0)[0]
+        #     win_ratio = len(n_win) / len(outcome)
+        #     bin_count += bin_size
+        #     X.append(bin_count)
+        #     Y.append(win_ratio)
+        # plt.plot(X, Y)
+        # plt.show()
 
         # ---- Visualize outcome ----
         # avg = self.moving_average(outcome_list, 10000)
@@ -178,10 +209,17 @@ class Agent:
                and states["player_count"][i_sample] <= 21 \
                and states["dealer_count"][i_sample] <= 11
 
+    @staticmethod
+    def get_samples(i_sample, states):
+        return states["playable_ace"][i_sample] == 0 \
+               and states["player_count"][i_sample] <= 21 \
+               and states["dealer_count"][i_sample] <= 11
+
     def visualize_action_surface(self):
-        # Add visualization for not count cards
-        # Refactor
+        # TODO: Add visualization for not count cards
+        # TODO: Refactor
         show_actions = False  # Show either actions or w-values
+        azim = 0
 
         state_vectors = self.extract_state_vectors(self.q)
         has_deck_count = len(state_vectors) > 5
@@ -231,12 +269,77 @@ class Agent:
                 ax.set_xlabel('dealer_count')
                 ax.set_ylabel('player_count')
                 ax.set_zlabel('Hit or stay')
-                ax.view_init(elev=55, azim=30)
                 mng = plt.get_current_fig_manager()
                 mng.window.state('zoomed')
                 ax.set_zlim([-1, 1])
-                plt.savefig('action_surface_' + str(n_image) + '.png')
+                basename = 'action_surface_' if show_actions else 'q_surface_'
+                plt.savefig(basename + str(n_image) + '.png')
                 n_image += 1
+                ax.view_init(elev=25, azim=azim)
+                # for i_im in range(9):
+                #     im_name = '00' + str(n_image) if n_image < 10 else '0' + str(n_image) if n_image < 100 else str(n_image)
+                #     ax.view_init(elev=25, azim=azim)
+                #     if azim != 360:
+                #         plt.savefig(basename + im_name + '.png')
+                #         #plt.savefig(basename + str(n_image) + '.png')
+                #     n_image += 1
+                #     azim += 1
+
+    def visualize_progress(self, outcome, episode_count, azim):
+        idx = np.where(np.array(outcome) != 0)[0]
+        outcome = np.array([o for i, o in enumerate(outcome) if i in idx])
+        n_win = np.where(outcome > 0)[0]
+        win_ratio = len(n_win) / len(outcome)
+
+        show_actions = False
+        state_vectors = self.extract_state_vectors(self.q)
+        im_name = datetime.datetime.today().strftime('%Y-%m-%d-%H:%M:%S').replace('-', '_').replace(':', '_')
+        n_states = len(state_vectors['player_count'])
+
+        valid_index = list(filter(lambda i_sample: self.get_samples(i_sample, state_vectors), range(0, n_states)))
+        player_count = np.array(state_vectors["player_count"])[valid_index]
+        dealer_count = np.array(state_vectors["dealer_count"])[valid_index]
+        action = np.array(state_vectors["action"])[valid_index]
+        q_val = np.array(state_vectors["max_q_val"])[valid_index]
+
+        player_count_unique = sorted(list(set(player_count)))
+        dealer_count_unique = sorted(list(set(dealer_count)))
+        hit_stay = np.zeros(len(player_count_unique) * len(dealer_count_unique)).reshape(
+            len(player_count_unique), len(dealer_count_unique))
+
+        player_count = list(player_count)
+        dealer_count = list(dealer_count)
+        action = list(action)
+        for idx_pcount, p_count in enumerate(player_count_unique):
+            for idx_dcount, d_count in enumerate(dealer_count_unique):
+                action_value = [x for i, x in enumerate(action) if
+                                dealer_count[i] == d_count and player_count[i] == p_count]
+                q_value = [x for i, x in enumerate(q_val) if
+                           dealer_count[i] == d_count and player_count[i] == p_count]
+                if show_actions:
+                    if action_value:
+                        hit_stay[idx_pcount][idx_dcount] = action_value[0]
+                    else:
+                        hit_stay[idx_pcount][idx_dcount] = np.nan
+                else:
+                    if q_value:
+                        hit_stay[idx_pcount][idx_dcount] = q_value[0]
+                    else:
+                        hit_stay[idx_pcount][idx_dcount] = 0
+        # ---- Visualize grid ----
+        x, y = np.meshgrid(dealer_count_unique, player_count_unique)
+        ax = plt.axes(projection="3d")
+        ax.plot_surface(x, y, hit_stay, rstride=1, cstride=1, cmap='winter', edgecolor='none')
+        ax.set_title('Win-ratio: ' + str(round(win_ratio*100)/100) + ', episode count: ' + str(episode_count))
+        ax.set_xlabel('dealer_count')
+        ax.set_ylabel('player_count')
+        ax.set_zlabel('Hit or stay')
+        mng = plt.get_current_fig_manager()
+        mng.window.state('zoomed')
+        ax.set_zlim([-1, 1])
+        ax.view_init(elev=25, azim=azim)
+        plt.savefig(im_name + '.png')
+        time.sleep(1)
 
     def evaluate_performance(self):
         policy = ['optimal', 'strict', 'random']
