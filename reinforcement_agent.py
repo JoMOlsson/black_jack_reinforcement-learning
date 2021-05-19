a3c11a0aa24b0781fac18979c6d1602a5e3d22b0
Joakim Olsson <joakim.michael.olsson@gmail.com>
2021-05-19 15:52:19 +0200

    Adding training visualisation

--- reinforcement_agent.py
+++ reinforcement_agent.py
@@ -93,6 +93,7 @@ class Agent:
         return mov_average
 
     def train_agent(self, num_of_episodes=1000000, azim_start=0, extract_performance_every=np.inf):
+        win_loss_ratio_list = []
         azim = azim_start                            # Azim angle start, used for performance visualisation
         q = self.q                                   # Get Q-table
         explore_stop = round(num_of_episodes * 0.8)  # Stop exploration after 80% of training
@@ -162,41 +163,17 @@ class Agent:
                     action = self.change_action(action, self.exploration_prob)
             outcome_list[i_episode] = reward
             if performance_count > extract_performance_every:
-                performance_count = 0
-                o = outcome_list[i_episode - extract_performance_every + 1:i_episode]
-                self.visualize_progress(o, i_episode, azim)
+                self.q = q             # Assign q-table
+                performance_count = 0  # re-initialize counter
+                win_loss_ratio = self.get_performance(number_of_episodes=100_000)
+                win_loss_ratio_list.append(win_loss_ratio)
+                self.visualize_progress(win_loss_ratio, win_loss_ratio_list, i_episode, azim)
                 azim += 1
 
         # ---- Save q-table ----
         with open('Q.json', 'w') as outfile:
             json.dump(q, outfile)
         self.q = q
-
-        # TODO: Put this in a function
-        # bin_size = 1000
-        # n = math.ceil(len(outcome_list) / bin_size)
-        # bin_count = 0
-        # X = []
-        # Y = []
-        # for i in range(n):
-        #     if i < n - 1:
-        #         outcome = np.array(outcome_list[bin_count:bin_count + bin_size])
-        #     else:
-        #         outcome = np.array(outcome_list[bin_count:-1])
-        #     idx = np.where(outcome != 0)[0]
-        #     outcome = np.array([o for i, o in enumerate(outcome) if i in idx])
-        #     n_win = np.where(outcome > 0)[0]
-        #     win_ratio = len(n_win) / len(outcome)
-        #     bin_count += bin_size
-        #     X.append(bin_count)
-        #     Y.append(win_ratio)
-        # plt.plot(X, Y)
-        # plt.show()
-
-        # ---- Visualize outcome ----
-        # avg = self.moving_average(outcome_list, 10000)
-        # plt.plot(avg)
-        # #plt.show()
 
         self.visualize_action_surface()
         self.evaluate_performance()
@@ -215,7 +192,7 @@ class Agent:
                and states["player_count"][i_sample] <= 21 \
                and states["dealer_count"][i_sample] <= 11
 
-    def visualize_action_surface(self):
+    def visualize_action_surface(self, rotation=True):
         # TODO: Add visualization for not count cards
         # TODO: Refactor
         show_actions = False  # Show either actions or w-values
@@ -272,25 +249,24 @@ class Agent:
                 mng = plt.get_current_fig_manager()
                 mng.window.state('zoomed')
                 ax.set_zlim([-1, 1])
-                basename = 'action_surface_' if show_actions else 'q_surface_'
-                plt.savefig(basename + str(n_image) + '.png')
-                n_image += 1
-                ax.view_init(elev=25, azim=azim)
-                # for i_im in range(9):
-                #     im_name = '00' + str(n_image) if n_image < 10 else '0' + str(n_image) if n_image < 100 else str(n_image)
-                #     ax.view_init(elev=25, azim=azim)
-                #     if azim != 360:
-                #         plt.savefig(basename + im_name + '.png')
-                #         #plt.savefig(basename + str(n_image) + '.png')
-                #     n_image += 1
-                #     azim += 1
 
-    def visualize_progress(self, outcome, episode_count, azim):
-        idx = np.where(np.array(outcome) != 0)[0]
-        outcome = np.array([o for i, o in enumerate(outcome) if i in idx])
-        n_win = np.where(outcome > 0)[0]
-        win_ratio = len(n_win) / len(outcome)
+                if not rotation:
+                    basename = 'action_surface_' if show_actions else 'q_surface_'
+                    plt.savefig(basename + str(n_image) + '.png')
+                    n_image += 1
+                    ax.view_init(elev=25, azim=azim)
+                else:
+                    for i_im in range(9):
+                        im_name = '00' + str(n_image) if n_image < 10 \
+                            else '0' + str(n_image) if n_image < 100 \
+                            else str(n_image)
+                        ax.view_init(elev=25, azim=azim)
+                        if azim != 360:
+                            plt.savefig(basename + im_name + '.png')
+                        n_image += 1
+                        azim += 1
 
+    def visualize_progress(self, win_ratio, win_loss_ratio_list, episode_count, azim, include_evaluation_plot=True):
         show_actions = False
         state_vectors = self.extract_state_vectors(self.q)
         im_name = datetime.datetime.today().strftime('%Y-%m-%d-%H:%M:%S').replace('-', '_').replace(':', '_')
@@ -327,19 +303,94 @@ class Agent:
                     else:
                         hit_stay[idx_pcount][idx_dcount] = 0
         # ---- Visualize grid ----
-        x, y = np.meshgrid(dealer_count_unique, player_count_unique)
-        ax = plt.axes(projection="3d")
-        ax.plot_surface(x, y, hit_stay, rstride=1, cstride=1, cmap='winter', edgecolor='none')
-        ax.set_title('Win-ratio: ' + str(round(win_ratio*100)/100) + ', episode count: ' + str(episode_count))
-        ax.set_xlabel('dealer_count')
-        ax.set_ylabel('player_count')
-        ax.set_zlabel('Hit or stay')
-        mng = plt.get_current_fig_manager()
-        mng.window.state('zoomed')
-        ax.set_zlim([-1, 1])
-        ax.view_init(elev=25, azim=azim)
-        plt.savefig(im_name + '.png')
-        time.sleep(1)
+
+        if not include_evaluation_plot:
+            axs = plt.axes(projection="3d")
+            axs.plot_surface(x, y, hit_stay, rstride=1, cstride=1, cmap='winter', edgecolor='none')
+            axs.set_zlim(-1, 1)
+            axs.set_title(f'Win-ratio: {round(win_ratio * 100) / 100} , episode count: {episode_count}', fontsize=18)
+            axs.set_xlabel('dealer_count', fontsize=12)
+            axs.set_ylabel('player_count', fontsize=12)
+            axs.set_zlabel('Hit or stay', fontsize=12)
+            axs.view_init(elev=25, azim=azim)
+            mng = plt.get_current_fig_manager()
+            mng.window.state('zoomed')
+            plt.savefig(im_name + '.png')
+        else:
+            fig = plt.figure(figsize=plt.figaspect(0.5))
+            axs = fig.add_subplot(1, 2, 1, projection="3d")
+            x, y = np.meshgrid(dealer_count_unique, player_count_unique)
+            surf = axs.plot_surface(x, y, hit_stay, rstride=1, cstride=1, cmap='winter',
+                                    linewidth=0, antialiased=False)
+            axs.set_zlim(-1, 1)
+            axs.set_title(f'Win-ratio: {round(win_ratio*100)/100} , episode count: {episode_count}',  fontsize=18)
+            axs.set_xlabel('dealer_count',  fontsize=12)
+            axs.set_ylabel('player_count',  fontsize=12)
+            axs.set_zlabel('Hit or stay',  fontsize=12)
+            axs.view_init(elev=25, azim=azim)
+            fig.colorbar(surf, shrink=0.5, aspect=10)
+
+            mng = plt.get_current_fig_manager()
+            mng.window.state('zoomed')
+
+            axs = fig.add_subplot(1, 2, 2)
+            evaluation_rounds = list(range(len(win_loss_ratio_list)))
+            axs.plot(evaluation_rounds, win_loss_ratio_list)
+            axs.set_xlabel('Evaluation round',  fontsize=12)
+            axs.set_ylabel('Win/loss ratio (%)',  fontsize=12)
+            axs.set_ylim(30, 50)
+            axs.set_xlim(0, len(evaluation_rounds))
+            plt.savefig(im_name + '.png')
+            plt.close(fig)
+
+    def get_performance(self, number_of_episodes=100_000):
+        win_list = [0] * number_of_episodes
+        for i in range(0, number_of_episodes):
+            player_cards, player_count, dealer_cards, dealer_count, playable_ace_p = self.dealer.new_game()
+            player_count_tag = self.dealer.construct_count_tag(player_count)
+            dealer_count_tag = self.dealer.construct_count_tag(dealer_count)
+
+            if self.COUNT_CARDS:
+                state = f"{player_count_tag}_{playable_ace_p}_{dealer_count_tag}_{self.dealer.deck_count}"
+            else:
+                state = f"{player_count_tag}_{playable_ace_p}_{dealer_count_tag}"
+
+            # Add state to Q-table
+            if state not in self.q:
+                self.q[state] = [0, 0]
+            q_vals = np.array(self.q[state])
+            action = np.argmax(q_vals)
+
+            game_over = False
+            while not game_over:
+                # Take action
+                reward, player_cards, player_count, dealer_cards, dealer_count, playable_ace_p, game_over = \
+                    self.dealer.take_action(action, player_cards, dealer_cards)
+                player_count_tag = self.dealer.construct_count_tag(player_count)
+                dealer_count_tag = self.dealer.construct_count_tag(dealer_count)
+                if self.COUNT_CARDS:
+                    new_state = f"{player_count_tag}_{playable_ace_p}_{dealer_count_tag}_{self.dealer.deck_count}"
+                else:
+                    new_state = f"{player_count_tag}_{playable_ace_p}_{dealer_count_tag}"
+
+                if game_over:
+                    q_new_state = [reward, reward]
+                elif new_state not in self.q:
+                    q_new_state = [0, 0]
+                    self.q[new_state] = q_new_state
+                else:
+                    q_new_state = self.q[new_state]
+
+                # Move to new state
+                q_vals = np.array(q_new_state)
+                action = np.argmax(q_vals)
+
+            win_list[i] = reward
+        # Calculate win-loss ratio
+        win_loss = np.where(np.array(win_list) != 0)[0]
+        wins = np.where(np.array(win_list)[win_loss] == 1)[0]
+        win_loss_ratio = round((len(wins) / len(win_loss)) * 1000) / 10
+        return win_loss_ratio
 
     def evaluate_performance(self):
         policy = ['optimal', 'strict', 'random']
